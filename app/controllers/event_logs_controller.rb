94232830e64d58fe661791b53dc8c5f31eee76e3
Shree <narayan.1785@gmail.com>
2021-12-01 15:24:47 +0530

    event log fix

--- app/controllers/event_logs_controller.rb
+++ app/controllers/event_logs_controller.rb
@@ -2,7 +2,7 @@ class EventLogsController < ApplicationController
 
   # Get all event logs for a given day
   def index
-	@event_logs = EventLog.get_event_logs.order("updated_at DESC").page params[:page]
+	@event_logs = EventLog.get_event_logs(params[:time_tracker_id]).order("updated_at DESC").page params[:page]
   end
 
 end
